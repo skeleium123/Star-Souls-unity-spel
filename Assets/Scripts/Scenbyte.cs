8a878eff6f5fd691a8d6effd246e240ae127d92c
h19mili <h19mili@axevalla.fhsk.se>
2024-01-29 15:28:56 +0100

    Gjorde en fungerande start knapp

---
+++ Assets/Scripts/Scenbyte.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Scenbyte : MonoBehaviour
+{
+    public string nextScene; 
+
+    public Button scenbyte;
+    // Start is called before the first frame update
+    void Start(){
+        Button btn = scenbyte.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        
+    }
+
+    // Update is called once per frame
+    void TaskOnClick(){
+
+        SceneManager.LoadScene(nextScene);
+        Debug.Log("You have clicked the button!");
+
+    }
+}
