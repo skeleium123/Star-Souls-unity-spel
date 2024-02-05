95b03b98e7a73dd4b101068606cb6392720dd56b
Cookie109 <h21cadi@axevalla.fhsk.se>
2024-02-05 15:36:08 +0100

    Trying to add enemy AI. Leave Alone.

---
+++ Assets/Scripts/AI Scripts/Adolf Hitler AI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class genericEnemyAI : MonoBehaviour
+{
+
+    //Följ Tutorial https://www.youtube.com/watch?v=UjkSFoLxesw 1:47
+
+    public NavMeshAgent Agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackrange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check for Sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+    }
+    private void Patroling()
+    {
+
+    }
+    private void chasePlayer()
+    {
+
+    }
+    private void attackPlayer()
+    {
+
+    }
+}
