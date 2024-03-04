32346eb32a708c9cff0ae0319aebe25e0b13bfab
Cookie109 <h21cadi@axevalla.fhsk.se>
2024-03-04 14:05:23 +0100

    Nytt Punkt system för navigering. Använder Navmesh.

--- Assets/Scripts/AI Scripts/GenericEnemyAI.cs
+++ Assets/Scripts/AI Scripts/GenericEnemyAI.cs
@@ -7,8 +7,51 @@ using UnityEngine.AI;
 
 public class genericEnemyAI : MonoBehaviour
 {
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
 
-    //Följ Tutorial https://www.youtube.com/watch?v=UjkSFoLxesw 1:47
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+
+
+
+
+
+
+    /*Följ Tutorial https://www.youtube.com/watch?v=UjkSFoLxesw 1:47
 
     public NavMeshAgent Agent;
 
@@ -133,5 +176,5 @@ public class genericEnemyAI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, attackrange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
-    }
+    }*/
 }
